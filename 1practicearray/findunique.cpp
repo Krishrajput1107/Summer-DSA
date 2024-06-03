0b93410fc719fbd18b419ea2e9d1fb7c0a6da375
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-03 23:11:00 +0530

    Create findunique.cpp

---
+++ 1practicearray/findunique.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int uniqueelement(int arr[],int size){
+    
+}
+int main(){
+    int size;
+    cout<<"enter the size of the array: "<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter the elements in the array"<<endl;
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    uniqueelement(arr,size)
+}
