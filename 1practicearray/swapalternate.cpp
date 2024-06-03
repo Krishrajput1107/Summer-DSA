c02150ecfd084479e51017b45ff541877e4341b9
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-03 23:06:14 +0530

    asdf

--- 1practicearray/swapalternate.cpp
+++ 1practicearray/swapalternate.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[7]={1,2,3,4,5,6,7};
+    int arr[8]={1,2,3,4,5,6,7,8};
     int s=0;
     int e=s+1;
-    while(e<7){
-        
+    while(e<8){
+        swap(arr[s],arr[e]);
+        s=s+2;
+        e=e+2;
+    }
+    for(int i=0;i<8;i++){
+        cout<<arr[i]<<" ";
     }
 }
