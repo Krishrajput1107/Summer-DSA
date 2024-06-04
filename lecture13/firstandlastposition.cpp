5e4944a32b0b305087c16e54d9ad7b89d078cf71
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-04 23:31:22 +0530

    wsedf

--- lecture13/firstandlastposition.cpp
+++ lecture13/firstandlastposition.cpp
@@ -1,23 +1,34 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 vector<int> searchRange(vector<int> &arr, int x)
 {
 	// Write your code here.
-	int firstocc=0,lastocc=0;
+	int firstocc,lastocc;
+    vector<int> sol;
 	for(int i=0;i<arr.size();i++){
 		if(arr[i]==x){
 			firstocc=i;
+            sol.push_back(firstocc);
 			break;
 		}
 	}
-	arr.push_back(firstocc);
 	for(int i=0;i<arr.size();i++){
 		if(arr[i]==x){
 			lastocc=i;
 		}
 	}
-	arr.push_back(lastocc);
+	sol.push_back(lastocc);
+    return sol;
 }
 int main(){
+    vector<int> arr={1,2,3,4,2,6,3};
+    int x;
+    cout<<"Enter the value of x: "<<endl;
+    cin>>x;
+    vector<int> ans=searchRange(arr,x);
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
 
 }
