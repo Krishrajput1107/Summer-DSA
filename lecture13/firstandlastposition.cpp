9068828d0f511b3fad289e4d9460a61516fe6048
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-04 23:19:22 +0530

    wef

--- lecture13/firstandlastposition.cpp
+++ lecture13/firstandlastposition.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 using namespace std;
+vector<int> searchRange(vector<int> &arr, int x)
+{
+	// Write your code here.
+	int firstocc=0,lastocc=0;
+	for(int i=0;i<arr.size();i++){
+		if(arr[i]==x){
+			firstocc=i;
+			break;
+		}
+	}
+	arr.push_back(firstocc);
+	for(int i=0;i<arr.size();i++){
+		if(arr[i]==x){
+			lastocc=i;
+		}
+	}
+	arr.push_back(lastocc);
+}
 int main(){
-    
+
 }
