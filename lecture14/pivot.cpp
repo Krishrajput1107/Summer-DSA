1e92dd160230c5efe8323576712582aa6b45922b
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-05 23:20:55 +0530

    dfghj

--- lecture14/pivot.cpp
+++ lecture14/pivot.cpp
@@ -5,11 +5,17 @@ int pivotelement(int arr[],int size){
     int mid=s+(e-s)/2;
     while(s<e){
         if(arr[mid]>arr[0]){
-            
+            s=mid+1;
         }
+        else{
+            e=mid;
+        }
+        mid=s+(e-s)/2;
     }
+    return s;
 }
 int main(){
-    int arr[7]={10,11,12,15,17,1,2};
-    pivotelement(arr,7);
+    int arr[7]={2,10,11,12,15,17,1};
+    int ans=pivotelement(arr,7);
+    cout<<"Index of Pivot element is: "<<ans<<endl;
 }
