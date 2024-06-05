e10bf9ded3900375f96b6bf994c8dbdcf062e8ee
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-05 23:17:53 +0530

    wedfg

--- lecture14/pivot.cpp
+++ lecture14/pivot.cpp
@@ -1,1 +1,15 @@
 #include<iostream>
+using namespace std;
+int pivotelement(int arr[],int size){
+    int s=0,e=size-1;
+    int mid=s+(e-s)/2;
+    while(s<e){
+        if(arr[mid]>arr[0]){
+            
+        }
+    }
+}
+int main(){
+    int arr[7]={10,11,12,15,17,1,2};
+    pivotelement(arr,7);
+}
