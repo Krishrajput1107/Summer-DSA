0cffc7256a3622334d8f40898dc16c60b85d5994
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-08 17:23:22 +0530

    XCVB

--- Sorting/Selectionsort.cpp
+++ Sorting/Selectionsort.cpp
@@ -1,32 +1,35 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void SelectionSort(vector<int> arr, int size){
-    for(int i=0;i<size-1;i++){
+vector<int> SelectionSort(vector<int> arr){
+    for(int i=0;i<arr.size()-1;i++){
         int minIndex=i;
-        for(int j=i+1;j<size;j++){
+        for(int j=i+1;j<arr.size();j++){
             if(arr[j]<arr[minIndex]){
                 minIndex=j;
             }
         }
         swap(arr[minIndex],arr[i]);
     }
+    return arr;
 }
-void printArray(vector<int> arr,int size){
+void printArray(vector<int> arr){
     cout<<"Sorted array is: "<<endl;
-    for(int i=0;i<size;i++){
-        cout<<arr[i];
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
     }
 }
 int main(){
     vector<int> arr;
-    int size;
-    cout<<"Enter the size of vector "<<endl;
-    cin>>size;
-    cout<<"Enter elements in the vector: "<<endl;
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
-   SelectionSort(arr,size);
-   printArray(arr,size);
+    arr.push_back(3);
+    arr.push_back(11);
+    arr.push_back(8);
+    arr.push_back(1);
+    arr.push_back(4);
+    arr.push_back(5);
+    arr.push_back(9);
+    arr.push_back(17);
+    arr.push_back(19);
+   vector<int> ans=SelectionSort(arr);
+   printArray(ans);
 }
