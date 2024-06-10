4851351256e08910947d4d5edb28b301aeb16c70
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-10 23:40:47 +0530

    ghj

---
+++ lecture22/replacespacewith@.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+void replacefun(string s){
+    string ans="";
+    for(int i=0;i<s.size();i++){
+        if(s[i] != ' '){
+            ans+=s[i];
+        }
+        else{
+            ans+="@40";
+        }
+    }
+    cout<<ans;
+}
+int main(){
+    string s;
+    cout<<"Enter any string: "<<endl;
+    cin>>s;
+    getline(cin,s);
+    replacefun(s);
+}
