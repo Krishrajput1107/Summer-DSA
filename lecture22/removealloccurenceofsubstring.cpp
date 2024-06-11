01b5b9f1e6551df922163da756d52269820987c1
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-11 12:51:01 +0530

    wedf

--- lecture22/removealloccurenceofsubstring.cpp
+++ lecture22/removealloccurenceofsubstring.cpp
@@ -2,7 +2,12 @@
 #include<string>
 #include<vector>
 using namespace std;
-
+void removesubstring(string s,string part){
+    while(s.length()!=0 && part.length()<s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    cout<<"Answer after removing substring: "<<s;
+}
 int main(){
     string s,part;
     cout<<"Enter any string: "<<endl;
