d8f6ba9bee4494c1b3801dd9095618115a6c9bd5
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-12 12:36:05 +0530

    sdfghj

--- lecture23/printlikeawave.cpp
+++ lecture23/printlikeawave.cpp
@@ -1,25 +1,43 @@
 #include<iostream>
 #include<array>
+#include<vector>
 using namespace std;
-void colwisesum(int arr[][4],int m,int n){
-    int sum;
+vector<int> wave(int arr[][4],int m,int n){
+    vector<int> ans;
     for(int col=0;col<n;col++){
-        sum=0;
-        for(int row=0;row<m;row++){
-            sum=sum+arr[row][col];
+        if(col&1){
+            //printing bottome to up
+            for(int row=m-1;row>=0;row--){
+                cout<<arr[row][col]<<" ";
+                ans.push_back(arr[row][col]);
+            }
+            cout<<endl;
         }
-        cout<<"Sum of "<<col<<"th col : "<<sum<<endl;
+        else{
+            //printing up to bottom
+            for(int row=0;row<m;row++){
+                cout<<arr[row][col]<<" ";
+                ans.push_back(arr[row][col]);
+            }
+            cout<<endl;
+        }
     }
+    return ans;
+    
 }
 
 int main(){
     int arr[3][4];
     cout<<"Enter elements in array: "<<endl;
     //taking input row-wise
-    for(int col=0;col<4;col++){
-        for(int row=0;row<3;row++){
+    for(int row=0;row<3;row++){
+        for(int col=0;col<4;col++){
             cin>>arr[row][col];
         }
     }
-    colwisesum(arr,3,4);
+    
+    vector<int> answer=wave(arr,3,4);
+    for(int i=0;i<answer.size();i++){
+        cout<<answer[i]<<" ";
+    }
 }
