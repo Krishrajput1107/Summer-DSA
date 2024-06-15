03dd7f528f06660c98a320d0d7cb8f3d64901e95
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-15 05:47:59 +0530

    xcvb

--- lecture26/introarrayptr.cpp
+++ lecture26/introarrayptr.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[10]={2,5,6};
+    int arr[10]={23,122,48,67};
     cout<<"Address of first block is: "<<arr<<endl;
     cout<<arr[0]<<endl;
     cout<<"Address of first block is: "<<&arr[0]<<endl;
@@ -12,5 +12,13 @@ int main(){
     cout<<"7th: "<<(*arr)+1<<endl;
     cout<<"8th: "<<arr[2]<<endl;
     cout<<"9th: "<<*(arr+2)<<endl;
+
+    int i=3;
+    cout<<i[arr]<<endl;
+
+    int temp[10];
+    int *ptr=&temp[0];
+    cout<<"Size of array: "<<sizeof(temp)<<endl;
+    cout<<"Size of pointer: "<< sizeof(ptr)<<endl;
 
 }
