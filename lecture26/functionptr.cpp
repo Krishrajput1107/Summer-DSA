d34e9ffccc73abd37e3ee9c8b9875ad53dbb105f
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-15 06:29:37 +0530

    vbnm

---
+++ lecture26/functionptr.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void print(int *p){
+    cout<< p <<endl;
+    cout<< *p <<endl;
+}
+void update(int *p){
+    p=p+1;
+    cout<<"inside: "<<p<<endl;
+
+}
+
+int main(){
+    int value=5;
+    int *p=&value;
+    //print(p);
+    cout<<"Before update: "<<p<<endl;
+    update(p);
+    cout<<"After update: "<<p<<endl;
+    return 0;
+}
