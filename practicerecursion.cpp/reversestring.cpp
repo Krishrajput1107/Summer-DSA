9a1f992aa4c8571024e23c4de194bc3aee991aa1
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-21 14:16:58 +0530

    cvb

---
+++ practicerecursion.cpp/reversestring.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int funcn(int arr[],int size){
+    
+}
+int main(){
+    int size;
+    cout<<"Enter size: "<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter elements: "<<endl;
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    funcn(arr,size);   
+}
