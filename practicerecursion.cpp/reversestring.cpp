5ce517cc08ab819e6a590ffe390a23aa4e89938b
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-21 14:24:38 +0530

    gh

--- practicerecursion.cpp/reversestring.cpp
+++ practicerecursion.cpp/reversestring.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 using namespace std;
-int funcn(int arr[],int size){
-    
+void reverse(string &s,int i,int j){
+    if(i>j){
+        return;
+    }
+    swap(s[i],s[j]);
+    i++;
+    j--;
+    return reverse(s,i,j);
 }
 int main(){
-    int size;
-    cout<<"Enter size: "<<endl;
-    cin>>size;
-    int arr[size];
-    cout<<"Enter elements: "<<endl;
-    for(int i=0;i<size;i++){
-        cin>>arr[i];
-    }
-    funcn(arr,size);   
+    string s;
+    cout<<"Enter string: "<<endl;
+    cin>>s;
+    int i=0;
+    int j=s.size()-1;
+    reverse(s,i,j); 
+    cout<< s <<endl;  
 }
