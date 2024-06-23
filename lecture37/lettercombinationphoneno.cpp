1f7132f9d6e90b3699617983f5b92d810b8ae269
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-23 12:54:44 +0530

    werfg

--- lecture37/lettercombinationphoneno.cpp
+++ lecture37/lettercombinationphoneno.cpp
@@ -1,8 +1,6 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-class Solution {
-private:
     void solve(string digit,vector<string> &ans,string output,int index,string mapping[]){
         if(index>=digit.length()){
             ans.push_back(output);
@@ -18,7 +16,6 @@ private:
         }
 
     }
-public:
     vector<string> letterCombinations(string digits) {
         vector<string> ans;
         string output;
@@ -29,6 +26,14 @@ public:
         string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
         solve(digits,ans,output,index,mapping);
         return ans;
-
     }
-};
+    int main(){
+        string digits;
+        cout<<"Enter digits: "<<endl;
+        cin>>digits;
+        vector<string> ans=letterCombinations(digits);
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
