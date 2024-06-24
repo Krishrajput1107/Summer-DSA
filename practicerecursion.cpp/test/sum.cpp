b6c73529e89c230de6c8e0a72a8b3f745e030a15
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-24 17:42:09 +0530

    wert

---
+++ practicerecursion.cpp/test/sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int Sum(int arr[],int size){
+    int sum=0;
+    if(size==0){
+        return 0;
+    }
+   return arr[0]+Sum(arr+1,size-1);
+}
+int main(){
+    int size;
+    cout<<"Enter size: "<<endl;
+    cin>>size;
+    int arr[size];
+    cout<<"Enter elements: "<<endl;
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    int ans=Sum(arr,size);
+    cout<<ans<<endl;
+}
