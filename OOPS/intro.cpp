d045b3e3c4b8fbaf43bbcfdb7567c9c5b9b8fe78
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-25 15:35:51 +0530

    asd

--- OOPS/intro.cpp
+++ OOPS/intro.cpp
@@ -2,10 +2,37 @@
 using namespace std;
 class Hero{
     //properties;
-    int health;
+    private:
+    int health=50;
+    public:
+    char level;
+    void print(){
+        cout<<level;
+    }
+    int gethealth(){
+        return health;
+    }
+    char getlevel(){
+        return level;
+    }
+    void sethealth(int h){
+        health=h;
+    }
+    void setlevel(char l){
+        level=l;
+    }
+
 };
 int main(){
     //creation of object
-    Hero h1;
-    cout<<"Size: "<<sizeof(h1)<<endl;
+    Hero baaki;
+    cout<<"Health is: "<<baaki.gethealth()<<endl;
+    baaki.sethealth(20);
+    cout<<"Health is: "<<baaki.gethealth()<<endl;
+    //cout<<"Health is: "<<baaki.sethealth(5)<<endl;
+    //baaki.health=60;
+    baaki.level='A';
+    cout<<"Size: "<<sizeof(baaki)<<endl;
+    //cout<<"Health: "<<baaki.health<<endl;
+    cout<<"level: "<<baaki.level<<endl;
 }
