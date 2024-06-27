9dc608130558e2b0ea5a15eb710dd17da04983e7
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-27 14:43:14 +0530

    xz

--- OOPS/Day1/intro2.cpp
+++ OOPS/Day1/intro2.cpp
@@ -25,8 +25,22 @@ class Hero{
 int main(){
     //static allocation
     Hero a;
+    a.sethealth(80);
+    a.setlevel('A');
+    cout<<"Level is:"<<a.level<<endl;
+    cout<<"Health is:"<<a.gethealth()<<endl;
 
     //Dynamic allocation
     Hero *b = new Hero;
+    b->sethealth(50);
+    b->setlevel('Z');
+
+    cout<<"Level is:"<<(*b).level<<endl;
+    cout<<"Health is:"<<(*b).gethealth()<<endl;
+
+    //alternate way to write
+    cout<<"Level is:"<<b->level<<endl;
+    cout<<"Health is:"<<b->gethealth()<<endl;
+
 
 }
