fc3c186877785728b4218c1ff1908e0bb5eae2b3
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-28 04:47:51 +0530

    fds

---
+++ OOPS/Day1/intro6.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+class Hero{
+    private:
+    int health;
+    public:
+    char level='Z';
+    int timetoComplete;
+    Hero(){
+        cout<<"Mae default constructor hu"<<endl;
+    }
+    //parameterized constructor
+    Hero(int health){
+        this->health=health;
+        cout<<"this->"<<this<<endl;
+    }
+    Hero(int health,char level){
+        this->health=health;
+        this->level=level;
+        cout<<"this is ad ->"<<this<<endl;
+    }
+    void print(){
+        cout<<level<<endl;
+    }
+    int gethealth(){
+        return health;
+    }
+    char getlevel(){
+        return level;
+    }
+    void sethealth(int h){
+        health=h;
+    }
+    void setlevel(char ch){
+        level=ch;
+    }
+    ~Hero(){
+        cout<<"Distructor called "<<endl;
+    }
+
+};
+int main(){
+    //static
+    Hero a;
+    //dynamic
+    Hero *b=new Hero;
+    delete b;
+
+}
