d76895762928941558392fa5dc7783bd69774706
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-27 10:38:22 +0530

    fcdxs

--- OOPS/Day2/inheritance.cpp
+++ OOPS/Day2/inheritance.cpp
@@ -9,21 +9,38 @@ class Human{
     int getAge(){
         return this->age;
     }
-    int setAge(int weight){
+    void setweight(int weight){
         this->weight=weight;
     }
 };
-class Male : public Human{
+class Male : protected Human{
     public:
     string colour;
     void sleep(){
         cout<<"Male sleeping "<<endl;
     }
+    int getHeight(){
+        return this->height;
+    }
 };
 int main(){
+    Male m1;
+    cout<<m1.getHeight()<<endl;
+
+
+
+
+
+
+
+    /*
     Male obj1;
     cout<< obj1.age<< endl;
     cout<< obj1.height<< endl;
     cout<< obj1.weight<< endl;
     cout<< obj1.colour<< endl;
+    obj1.sleep();
+    obj1.setweight(80);
+    cout<< obj1.weight<< endl;
+    */
 }
