0c2651b77e70dc3c269763c3e66a5e46212b5942
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-06-30 05:26:48 +0530

    fd

---
+++ lecture44/doublylinkedlis.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int d){
+        this->data=d;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+//traversing a linked list
+void print(Node* &head){
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+//get length
+int getLength(Node* &head){
+    int len=0;
+    Node *temp=head;
+    while(temp!=NULL){
+        len++;
+        temp=temp->next;
+    }
+    return len;
+}
+
+
+int main(){
+    Node *node1=new Node(10);
+    Node *head=node1;
+    print(head);
+    cout<<getLength(head);
+
+
+}
