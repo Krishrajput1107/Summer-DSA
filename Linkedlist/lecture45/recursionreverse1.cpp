0eb30a2b24a7de5e5f42d67d0e6ec808b27b730c
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-07-01 13:28:07 +0530

    frdsa

---
+++ Linkedlist/lecture45/recursionreverse1.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(int d){
+        this->data=d;
+        this->next=NULL;
+    }
+
+
+};
+void insertatTail(Node* &tail,int d){
+    Node *temp=new Node(d);
+    tail->next=temp;
+    tail=temp;
+}
+
+void print(Node* &head){
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+Node* reverse(Node* &head){
+    if((head==NULL) || (head->next==NULL)){
+        return head;
+    }
+    Node* chotaHead=reverse(head->next);
+    head->next->next=head;
+    head->next=NULL;
+    return chotaHead;
+
+}
+
+
+int main(){
+    Node *node1=new Node(10);
+    //cout<<node1->data<<endl;
+    //cout<<node1->next;
+    Node *tail=node1;
+    Node *head=node1;
+    print(head);
+    insertatTail(tail,15);
+    print(head);
+
+    insertatTail(tail,20);
+    print(head);
+
+    insertatTail(tail,23);
+    print(head);
+
+    insertatTail(tail,31);
+    print(head);
+
+    Node *ans=reverse(head);
+    print(ans);
+
+
+}
