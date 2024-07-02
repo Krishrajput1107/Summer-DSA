e15d081ec61a77226682b0bd90206d26cc0f0f66
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-07-02 13:55:10 +0530

    fds

---
+++ lecture47/detectcycle.cpp
@@ -0,0 +1,139 @@
+#include<iostream>
+#include<map>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    //constructor
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+
+    //distructor
+    ~Node(){
+        int value=this->data;
+        //MEMORY FREE
+        if(this->next!=NULL){
+            delete next;
+            this->next=NULL;
+        }
+        cout<<"memory is free for node with data "<<value<<endl;
+    }
+};
+void InsertatHead(Node* &head,int d){
+    Node *temp=new Node(d);
+    temp->next=head;
+    head=temp;
+
+}
+void InsertatTail(Node* &tail, int d){
+    Node *temp=new Node(d);
+    tail->next=temp;
+    tail=temp;
+
+}
+void print(Node* &head){
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void InsertAtposition(Node* &tail,Node * &head,int position,int d){
+    //insert at startposition
+    if(position==1){
+        InsertatHead(head,d);
+        return;
+    }
+    int cnt=1;
+    Node *temp=head;
+    while(cnt<position-1){
+        temp=temp->next;
+        cnt++;
+    }
+    //Insert at last position
+    if(temp->next==NULL){
+        InsertatTail(tail,d);
+        return;
+    }
+
+    //Insert at any position
+    Node *nodetoInsert=new Node(d);
+    nodetoInsert->next=temp->next;
+    temp->next=nodetoInsert;
+}
+
+void deleteNode(int position, Node* &head){
+    //delteing first node
+    if(position==1){
+        Node *temp=head;
+        head=head->next;
+        //memory free krdo
+        temp->next=NULL;
+        delete temp;
+    }
+    //deleting any mid or last node
+    else{
+        Node *current=head;
+        Node *prev=NULL;
+        int cnt=1;
+        while(cnt<position){
+            prev=current;
+            current=current->next;
+            cnt++;
+        }
+        prev->next=current->next;
+        current->next=NULL;
+        delete current;
+    }
+}
+bool detectCycle(Node* &head){
+    Node *temp=head;
+    map<Node*,bool> visited;
+    while(temp!=NULL){
+        if(visited[temp]==true){
+            return true;
+        }
+        visited[temp]=true;
+        temp=temp->next;
+    }
+    return false;
+}
+
+int main(){
+    //created a new node
+    Node *node1=new Node(10);
+    //cout<<node1->data<<endl;
+    //cout<<node1->next<<endl;
+
+    //head pointed to node 1
+    Node *head=node1;
+    Node *tail=node1;
+    print(head);
+    InsertatTail(tail,12);
+    print(head);
+
+    InsertatTail(tail,15);
+    print(head);
+
+    InsertAtposition(tail,head,4,20);
+    print(head);
+
+    //cout<<head->data<<" ";
+    //cout<<tail->data<<" ";
+
+    tail->next=head->next;
+    bool ans=detectCycle(head);
+    if(ans){
+        cout<<"It is Cyclic"<<endl;
+    }
+    else{
+        cout<<"Not Cyclic"<<endl;
+    }
+
+    
+}
