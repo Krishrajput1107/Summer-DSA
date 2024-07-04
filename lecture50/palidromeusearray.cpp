aa90c3f46d63d2a8ef50f12905b5e0ca62f08e23
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-07-04 17:48:59 +0530

    fds

---
+++ lecture50/palidromeusearray.cpp
@@ -0,0 +1,142 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    //constructor
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+
+    //distructor
+    ~Node(){
+        int value=this->data;
+        //MEMORY FREE
+        if(this->next!=NULL){
+            delete next;
+            this->next=NULL;
+        }
+        cout<<"memory is free for node with data "<<value<<endl;
+    }
+};
+void InsertatHead(Node* &head,int d){
+    Node *temp=new Node(d);
+    temp->next=head;
+    head=temp;
+
+}
+void InsertatTail(Node* &tail, int d){
+    Node *temp=new Node(d);
+    tail->next=temp;
+    tail=temp;
+
+}
+void print(Node* &head){
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void InsertAtposition(Node* &tail,Node * &head,int position,int d){
+    //insert at startposition
+    if(position==1){
+        InsertatHead(head,d);
+        return;
+    }
+    int cnt=1;
+    Node *temp=head;
+    while(cnt<position-1){
+        temp=temp->next;
+        cnt++;
+    }
+    //Insert at last position
+    if(temp->next==NULL){
+        InsertatTail(tail,d);
+        return;
+    }
+
+    //Insert at any position
+    Node *nodetoInsert=new Node(d);
+    nodetoInsert->next=temp->next;
+    temp->next=nodetoInsert;
+}
+
+void deleteNode(int position, Node* &head){
+    //delteing first node
+    if(position==1){
+        Node *temp=head;
+        head=head->next;
+        //memory free krdo
+        temp->next=NULL;
+        delete temp;
+    }
+    //deleting any mid or last node
+    else{
+        Node *current=head;
+        Node *prev=NULL;
+        int cnt=1;
+        while(cnt<position){
+            prev=current;
+            current=current->next;
+            cnt++;
+        }
+        prev->next=current->next;
+        current->next=NULL;
+        delete current;
+    }
+}
+bool checkPalindrome(vector<int> arr){
+    int s=0;
+    int e=arr.size()-1;
+    while(s<e){
+        if(arr[s]!=arr[e]){
+            return false;
+        }
+        s++;
+        e--;
+    }
+    return true;
+}
+bool isPalindrome(Node* &head){
+    vector<int> arr;
+    Node* temp=head;
+    while(temp!=NULL){
+        arr.push_back(temp->data);
+        temp=temp->next;
+    }
+
+    return checkPalindrome(arr);
+
+}
+
+int main(){
+    //created a new node
+    Node *node1=new Node(1);
+    //cout<<node1->data<<endl;
+    //cout<<node1->next<<endl;
+
+    //head pointed to node 1
+    Node *head=node1;
+    Node *tail=node1;
+    InsertatTail(tail,2);
+
+    InsertatTail(tail,1);
+    print(head);
+
+    bool ans=isPalindrome(head);
+    if(ans){
+        cout<<"Linked list is palindrome "<<endl;
+    }
+    else{
+        cout<<"Not palindrome "<<endl;
+    }
+
+    
+   
+}
