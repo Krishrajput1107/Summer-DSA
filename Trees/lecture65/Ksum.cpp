ae39cb75cd16b5ac8c61a1786af96014cf2533dc
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-07-24 01:48:24 +0530

    dsa

---
+++ Trees/lecture65/Ksum.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <map>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildtree() {
+    int data;
+    cout << "Enter the data: " << endl;
+    cin >> data;
+
+    if (data == -1) {
+        return NULL;
+    }
+
+    Node* root = new Node(data);
+
+    cout << "Enter data in left of " << data << endl;
+    root->left = buildtree();
+
+    cout << "Enter data in right of " << data << endl;
+    root->right = buildtree();
+
+    return root;
+}
+
+void solve(Node* root, int k, vector<int>path, int &count){
+    if(root==NULL){
+        return;
+    }
+
+    path.push_back(root->data);
+
+    solve(root->left,k,path,count);
+    solve(root->right,k,path,count);
+
+    int size=path.size();
+    int sum=0;
+    for(int i=size-1;i>=0;i--){
+        sum=sum+path[i];
+        if(sum==k){
+            count++;
+        }
+    }
+
+    path.pop_back();
+}
+
+int Ksum(Node* root,int k){
+    vector<int> path;
+    int count=0;
+    solve(root, k, path, count);
+
+    return count;
+}
+
+int main() {
+    Node* root = NULL;
+    int k=5;
+    root = buildtree();
+
+    int ans = Ksum(root,k);
+
+    cout << "Number of path which have their sum equal to "<< k<<" is: " << ans << endl;
+
+    return 0;
+}
+
+
+
+// 1 3 2 -1 -1 1 1 -1 -1 -1 0 4 1 -1 -1 2 -1 -1 5 -1 6 -1 -1
