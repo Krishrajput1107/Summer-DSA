d43f537806f3f31a5b13b1861e306e823a085337
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-07-23 09:37:33 +0530

    dsa

---
+++ Trees/lecture64/VerticalTraversal.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+#include<map>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+
+};
+
+
+
+vector<int> verticalOrder(Node *root)
+    {
+        map<int, map<int,vector<int> > > nodes;
+        queue< pair<Node*, pair<int,int> > > q;
+        vector<int> ans;
+        
+        if(root == NULL)
+            return ans;
+            
+        q.push(make_pair(root, make_pair(0,0)));
+        
+        while(!q.empty()) {
+            pair<Node*, pair<int,int> > temp = q.front();
+            q.pop();
+            Node* frontNode = temp.first;
+            int hd = temp.second.first;
+            int lvl = temp.second.second;
+            
+            nodes[hd][lvl].push_back(frontNode->data);
+            
+            if(frontNode->left)
+                q.push(make_pair(frontNode->left, make_pair(hd-1, lvl+1) ));
+                
+            if(frontNode->right)
+                q.push(make_pair(frontNode->right, make_pair(hd+1, lvl+1)));
+        }
+        
+        for(auto i: nodes) {
+            
+            for(auto j:i.second) {
+                
+                for(auto k:j.second)
+                {
+                    ans.push_back(k);
+                }
+            }
+        }
+        return ans;
+    }
+
+
+Node* buildtree(Node* &root){
+    int data;
+    cout<<"Enter the data: "<<endl;
+    cin>>data;
+
+    if(data==-1){
+        return NULL;
+    }
+
+    root=new Node(data);
+
+    
+    cout<<"Insert data inserting at left of "<<data<<endl;
+    buildtree(root->left);
+
+    cout<<"Insert data inserting at right of "<<data<<endl;
+    buildtree(root->right);
+
+    return root;
+}
+
+
+int main(){
+    Node *root=NULL;
+    //1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    buildtree(root);
+
+    vector<int>answer=verticalOrder(root);
+    cout<<"Priniting Vertical order traversal: "<<endl;
+
+    for(int i=0;i<answer.size();i++){
+        cout<<answer[i]<<" ";
+    }
+
+    // 1 2 4 -1 -1 5 -1 -1 3 6 -1 8 -1 -1 7 -1 9 -1 -1 
+
+}
