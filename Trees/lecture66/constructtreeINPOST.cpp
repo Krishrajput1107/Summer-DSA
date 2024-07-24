dd4c8865eb9b0175ec7856c43a892010c8b8b3fa
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-07-25 03:18:47 +0530

    daaa

---
+++ Trees/lecture66/constructtreeINPOST.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include<map>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data=data;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+int findPosition(int in[],int element,int n){
+    for(int i=0;i<n;i++){
+        if(in[i]==element){
+            return i;
+        }
+    }
+    return -1;
+}
+
+Node* solve(int in[],int post[],int &index,int inorderStart,int inorderEnd,int n){
+    //base case if covered all elements of preorder
+    if(index<0 || inorderStart>inorderEnd){
+        return NULL;
+    }
+
+    int element=post[index--];
+    Node *root=new Node(element);
+    int position=findPosition(in,element,n);
+
+    //recursive calls
+    root->right=solve(in,post,index,position+1,inorderEnd,n);
+    root->left=solve(in,post,index,inorderStart,position-1,n);
+
+    return root;
+
+}
+
+Node* buildtree(int in[],int pre[], int n){
+    int postOrderIndex=n-1;
+    Node *ans=solve(in,pre,postOrderIndex,0,n-1,n);
+    return ans;
+}
+
+void preOrderTraversal(Node* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+int main(){
+    int in[]={4,8,2,5,1,6,3,7};
+    int post[]={8,4,5,2,6,7,3,1};
+    int n=8;
+    Node* ans=buildtree(in,post,n);
+
+    preOrderTraversal(ans);
+
+}
