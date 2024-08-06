a5d3c526d513252ec063772d3a76f8836d6dbe5f
Krishrajput1107 <120461221+Krishrajput1107@users.noreply.github.com>
2024-08-06 23:51:45 +0530

    daa

--- Trees/practice/practice66/Inpost.cpp
+++ Trees/practice/practice66/Inpost.cpp
@@ -26,43 +26,45 @@ int findPosition(int in[], int element, int size) {
     return -1;
 }
 
-Node* solve(int in[], int pre[], int &index, int inorderStart, int inorderEnd, int size) {
-    if (index >= size || inorderStart > inorderEnd) {
+Node* solve(int in[], int post[], int &index, int inorderStart, int inorderEnd, int size) {
+    if (index < 0 || inorderStart > inorderEnd) {
         return NULL;
     }
 
-    int element = pre[index++];
+    int element = post[index--];
     Node* root = new Node(element);
     int pos = findPosition(in, element, size);
 
-    root->left = solve(in, pre, index, inorderStart, pos - 1, size);
-    root->right = solve(in, pre, index, pos + 1, inorderEnd, size);
+    
+    root->right = solve(in, post, index, pos + 1, inorderEnd, size);
+    root->left = solve(in, post, index, inorderStart, pos - 1, size);
 
     return root;
 }
 
-Node* buildTree(int in[], int pre[], int n) {
-    int preorderIndex = 0;
-    Node* ans = solve(in, pre, preorderIndex, 0, n - 1, n);
+Node* buildTree(int in[], int post[], int n) {
+    int postorderIndex = n-1;
+    Node* ans = solve(in, post, postorderIndex, 0, n - 1, n);
     return ans;
 }
 
-void postOrderTraversal(Node* root) {
+void preOrderTraversal(Node* root) {
     if (root == NULL) {
         return;
     }
-    postOrderTraversal(root->left);
-    postOrderTraversal(root->right);
     cout << root->data << " ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+    
 }
 
 int main() {
-    int in[] = {3, 1, 4, 0, 5, 2};
-    int pre[] = {0, 1, 3, 4, 2, 5};
-    int n = 6;
-    Node* ans = buildTree(in, pre, n);
+    int in[] = {4,8,2,5,1,6,3,7};
+    int post[] = {8,4,5,2,6,7,3,1};
+    int n = 8;
+    Node* ans = buildTree(in, post, n);
 
-    postOrderTraversal(ans);
+    preOrderTraversal(ans);
 
     return 0;
 }
